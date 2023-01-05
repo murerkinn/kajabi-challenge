c841157bf3de7eb5ff6a0ec9e83e5dfd2e9313d3
Murat Erkin Çiçek <57751146+murerkinn@users.noreply.github.com>
2023-01-05 23:49:07 +0300

    Support filtering by email

--- src/pages/api/employees/index.ts
+++ src/pages/api/employees/index.ts
@@ -1,3 +1,4 @@
+import { omit } from 'lodash'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { ReqResAPI } from '@/lib/api'
@@ -22,9 +23,42 @@ export default async function handler(
     return
   }
 
-  const { data } = await ReqResAPI.get('/users')
+  const page = Number(req.query.page || 1)
+  const per_page = Number(req.query.per_page || 6)
 
-  delete data.support
+  let result: Data
 
-  res.status(200).json(data)
+  if (req.query.email) {
+    const { data } = await ReqResAPI.get('/users')
+
+    const { data: employeeData } = await ReqResAPI.get(`/users`, {
+      params: {
+        per_page: data.total,
+      },
+    })
+
+    const employees = employeeData.data.filter(
+      (employee: Employee) =>
+        employee.email.search(req.query.email as string) !== -1
+    )
+
+    result = {
+      data: employees.slice(page == 1 ? 0 : per_page * page, per_page),
+      page,
+      per_page,
+      total: employees.length,
+      total_pages: Math.ceil(employees.length / per_page),
+    }
+  } else {
+    const { data } = await ReqResAPI.get('/users', {
+      params: {
+        page,
+        per_page,
+      },
+    })
+
+    result = omit(data, ['support']) as Data
+  }
+
+  res.status(200).json(result)
 }
